4ba1bdaebbebbfa402d21ca7e77f95d64db30496
Abdumalik <AbdulmalikMashrab@gmail.com>
2022-04-17 10:39:07 +0500

    Add commit

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const scoreCounter = document.querySelector('.score-counter')
-const randomWordEl= document.querySelector('.random-word')
+const randomWordEl = document.querySelector('.random-word')
 const textInput = document.querySelector('.text-input')
-const time = document.querySelector('.time')
+const timeEl = document.querySelector('.time')
 
 const api = 'https://random-words-api.vercel.app/word'
 
 let randomWord
 
+let score = 0
+
+let time = 10
+
 function get() {
-    fetch(api)
-    .then((data)=> {
-        return data.json() 
+  fetch(api)
+    .then((word) => {
+      return word.json()
     })
     .then(getWord)
 }
@@ -18,13 +22,27 @@ function get() {
 get()
 
 function getWord(e) {
-    randomWord = e[0].word.toLowerCase()
-    randomWordEl.textContent = randomWord
+  randomWord = e[0].word.toLowerCase()
+  randomWordEl.textContent = randomWord
 }
 
-textInput.addEventListener('input', ()=> {
-    if(randomWord == textInput.value) {
-        get()
-        textInput.value = ''
-    }
+textInput.addEventListener('input', () => {
+  if (randomWord == textInput.value) {
+    get()
+    textInput.value = ''
+    score++
+    scoreCounter.textContent = score
+    time += 3
+  }
 })
+
+setInterval(() => {
+  if (time) {
+    time--
+    timeEl.textContent = time
+
+    // modal => game over
+    // score: 9
+    // button refresh sahifa
+  }
+}, 1000)
